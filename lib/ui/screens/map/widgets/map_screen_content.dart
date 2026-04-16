444d95e61c68af6566cfda363b1a9e2657a662f4
Chhun-Nika <chhunnika488@gmail.com>
2026-04-16 11:17:34 +0700

    US02 - 01 : google map setup and testing

--- lib/ui/screens/map/widgets/map_screen_content.dart
+++ lib/ui/screens/map/widgets/map_screen_content.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreenContent extends StatelessWidget {
   const MapScreenContent({super.key});
+
+  static const CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(11.5564, 104.9282), // Phnom Penh
+    zoom: 14,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Map"));
+    return const GoogleMap(
+      initialCameraPosition: _initialPosition,
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+    );
   }
 }
