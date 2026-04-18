b9efda843132b15444c779345e21161655881f6e
Chhun-Nika <chhunnika488@gmail.com>
2026-04-18 08:08:35 +0700

    US02 - 05 : polish ui

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -30,17 +30,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _currentIndex = 0;
 
-  final List<Widget> _pages = const [MapScreen(), BookingHistoryScreen(),];
+  final List<Widget> _pages = const [MapScreen(), BookingHistoryScreen()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(index: _currentIndex, children: _pages),
+      body: _pages[_currentIndex],
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           border: Border(
             top: BorderSide(
-              color: Color.fromARGB(255, 233, 233, 233), // or your theme color
+              color: Color.fromARGB(255, 233, 233, 233),
               width: 0.2,
             ),
           ),
@@ -50,12 +50,15 @@ class _MyAppState extends State<MyApp> {
           currentIndex: _currentIndex,
           onTap: (index) {
             setState(() {
-              _currentIndex = index;
+              _currentIndex = index; // triggers rebuild
             });
           },
           selectedItemColor: AppColor.primary,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.map_outlined), label: 'Map'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map_outlined),
+              label: 'Map',
+            ),
             BottomNavigationBarItem(
               icon: Icon(Icons.confirmation_num_outlined),
               label: 'Bookings',
